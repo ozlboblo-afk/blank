eab35e0630d66e3c2ce9dfe14f7f7a24668fc7aa
みあ <ozlb.oblo@gmail.com>
2026-07-12 20:00:50 +0900

    feat:全面リニューアル

--- js/humburger.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const headerNav = document.querySelector(".headerNav");
-
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("is-open");
-  headerNav.classList.toggle("is-open");
-});
